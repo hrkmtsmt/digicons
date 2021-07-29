9fd07c288a1c667101e60487a7e2886b611c3046
hrkmtsmt <hrkmtsmt@icloud.com>
2021-07-30 00:41:48 +0900

    :+1: feature: icon-box.js add functin clipboard

--- src/components/icon-box.tsx
+++ src/components/icon-box.tsx
@@ -7,14 +7,34 @@ type Props = {
 };
 
 export const IconBox = (props: Props) => {
-    const [onFocus, setOnFocus] = useState(false);
-    const toggleOnForcus = () => setOnFocus(!onFocus);
+    const [isFocus, setIsFocus] = useState(false);
+    const toggleOnForcus = () => setIsFocus(!isFocus);
     const toggleClass = () => {
-        if (onFocus === false) {
+        if (isFocus === false) {
             return "";
         } else {
             return "is-focus";
         }
+    };
+    const copyClipboad = (text: string) => {
+        navigator.clipboard.writeText(text).then(
+            () => {
+                const copiedSuccsesed: any =
+                    document.getElementById("copied_successed");
+                copiedSuccsesed.classList.add("is-visible");
+                setTimeout(() => {
+                    copiedSuccsesed.classList.remove("is-visible");
+                }, 4000);
+            },
+            () => {
+                const copiedFailed: any =
+                    document.getElementById("copied_failed");
+                copiedFailed.classList.add("is-visible");
+                setTimeout(() => {
+                    copiedFailed.classList.remove("is-visible");
+                }, 4000);
+            }
+        );
     };
     return (
         <React.Fragment>
@@ -25,21 +45,37 @@ export const IconBox = (props: Props) => {
                     onBlur={toggleOnForcus}
                     className={"c-icon-box "}
                 >
-                    <i className={`c-icon-box-content ${props.icon}`}>
+                    <i className={`c-icon-box-icon ${props.icon}`}>
                         {props.svg}
                     </i>
                     <span className={"c-icon-box-title"}>{props.title}</span>
                 </button>
-                <div className={`c-icon-bord ${toggleClass()}`}>
-                    <div className={"c-icon-bord-inner"}>
-                        <ul className={"c-nav-horizonal"}>
-                            <li className={"c-nav-item"}>
-                                <button className={"c-button"}>a</button>
-                            </li>
-                            <li className={"c-nav-item"}>
-                                <button className={"c-button-border"}>a</button>
-                            </li>
-                        </ul>
+                <div className={`c-icon-bord is-focus ${toggleClass()}`}>
+                    <div className={"c-icon-bord-body"}>
+                        <div className={"c-nav-horizonal"}>
+                            <i className={` ${props.icon}`}>{props.svg}</i>
+                            <span id={"s"} className={"c-icon-bord-title"}>
+                                {props.title}
+                            </span>
+                            <div
+                                id={"copied_successed"}
+                                className={"c-icon-bord-alert"}
+                            >
+                                Copied
+                            </div>
+                            <div
+                                id={"copied_failed"}
+                                className={"c-icon-bord-alert"}
+                            >
+                                Error
+                            </div>
+                        </div>
+                        <button
+                            onClick={() => copyClipboad(props.svg)}
+                            className={"c-button"}
+                        >
+                            Copy SVG
+                        </button>
                     </div>
                 </div>
             </div>
