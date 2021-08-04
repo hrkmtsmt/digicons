ac22aa9896c5008e2d4e913a3a9986b3f807f2bc
hrkmtsmt <hrkmtsmt@icloud.com>
2021-08-05 01:15:18 +0900

    :+1: feature: Loop.js map() in map()

--- src/components/Loop.js
+++ src/components/Loop.js
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Section } from "./section";
 import { IconBox } from "./icon-box";
-import Icons from "../image/icons.json";
-
+import axios from "axios";
 export const Loop = () => {
-  const category = Icons.icons[0].id;
+  const [icons, setIcons] = useState([]);
+  useEffect(() => {
+    const headers = { "X-API-KEY": process.env.REACT_APP_API_KEY };
+    const endpoint = `${process.env.REACT_APP_ENDPOINT}icons`;
+    axios.get(endpoint, { headers: headers }).then((res) => {
+      setIcons(res.data.contents);
+    });
+  }, []);
   return (
-    <Section title={category} desc={""}>
-      {Icons.icons[0].icon.map((icon) => (
-        <IconBox
-          key={icon.id}
-          id={icon.id}
-          title={icon.name}
-          svg={icon.element}
-          image={`src/image/${category}/${icon.id}.svg`}
-        />
+    <React.Fragment>
+      {icons.map((category, index) => (
+        <Section title={category.category} desc={""}>
+          {icons[index].icons.map((icon) => (
+            <IconBox
+              key={icon.id}
+              id={icon.id}
+              title={icon.name}
+              svg={icon.element}
+              image={icon.image.url}
+            />
+          ))}
+        </Section>
       ))}
-    </Section>
+    </React.Fragment>
   );
 };
